c8108fc9e259c69d12ae782e73f3587034285366
Touchebag <someone90_6@hotmail.com>
2014-05-23 19:15:11 +0200

    Can now print correct size of files and directories

--- parser.hs
+++ parser.hs
@@ -3,11 +3,12 @@ module Parser where
 import System.Directory
 import System.Posix
 
-data FileTree = File String FileStatus | Dir String FileStatus [FileTree]
+data FileTree = File String Int FileStatus | Dir String Int FileStatus [FileTree]
 
 instance Show FileTree where
-  show (File path _)   = "File: " ++ path
-  show (Dir  path _ files) = "Dir: "  ++ path ++ "\n" ++ unlines (map show files)
+  show (File path size _)   = "File: " ++ path ++ "\n Size: " ++ show size
+  show (Dir  path size _ files) = "Dir: "  ++ path ++
+                "\n Size: " ++ show size ++ "\n" ++ unlines (map show files)
 
 parseTree :: String -> IO FileTree
 parseTree path = do
@@ -16,17 +17,28 @@ parseTree path = do
         then do
           files <- getDirectoryContents path
           tree  <- mapM parseTree $ filterDirContents path files
-          return $ Dir path file tree
-        else return $ File path file
+          return $ Dir path (-1) file tree
+        else return $ File path (-1) file
 
 filterDirContents :: FilePath -> [FilePath] -> [FilePath]
 filterDirContents _    []        = []
 filterDirContents path (".":fs)  = filterDirContents path fs
 filterDirContents path ("..":fs) = filterDirContents path fs
-filterDirContents path (f:fs)    = (path ++ "/" ++ f) : filterDirContents path fs
+filterDirContents path (f:fs)    = (path ++ "/" ++ f) :
+                                    filterDirContents path fs
 
 getFileSize :: FileStatus -> Int
 getFileSize stat = read (show (fileSize stat)) :: Int
 
-getDirSize :: FileStatus -> Int
-getDirSize = undefined
+calculateTreeSize :: FileTree -> FileTree
+calculateTreeSize entry@(File path size stat) = if size == -1
+                  then File path (getFileSize stat) stat
+                  else entry
+calculateTreeSize entry@(Dir path size stat files) = if size == -1
+                  then Dir path (sum $ map getDirSize tree) stat tree
+                  else entry
+        where tree = map calculateTreeSize files
+
+getDirSize :: FileTree -> Int
+getDirSize (File _ size _)       = abs size
+getDirSize (Dir  _ _    _ files) = sum $ map getDirSize files

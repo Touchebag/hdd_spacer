f69a6cbd20a723513273b82bbb5957f1ae5ba0eb
Touchebag <someone90_6@hotmail.com>
2014-05-23 18:49:12 +0200

    Can parse directories (I think)

--- parser.hs
+++ parser.hs
@@ -1,19 +1,32 @@
 module Parser where
 
+import System.Directory
 import System.Posix
 
-data FileTree = File Int | Dir Int [FileTree]
-  deriving (Show)
+data FileTree = File String FileStatus | Dir String FileStatus [FileTree]
+
+instance Show FileTree where
+  show (File path _)   = "File: " ++ path
+  show (Dir  path _ files) = "Dir: "  ++ path ++ "\n" ++ unlines (map show files)
 
 parseTree :: String -> IO FileTree
 parseTree path = do
       file <- getFileStatus path
       if isDirectory file
-        then return $ File 3
-        else do
-          offSize <- getFileSize file
-          let size = read (show offSize) :: Int
-          return $ File size
+        then do
+          files <- getDirectoryContents path
+          tree  <- mapM parseTree $ filterDirContents path files
+          return $ Dir path file tree
+        else return $ File path file
 
-getFileSize :: FileStatus -> IO FileOffset
-getFileSize stat = return $ fileSize stat
+filterDirContents :: FilePath -> [FilePath] -> [FilePath]
+filterDirContents _    []        = []
+filterDirContents path (".":fs)  = filterDirContents path fs
+filterDirContents path ("..":fs) = filterDirContents path fs
+filterDirContents path (f:fs)    = (path ++ "/" ++ f) : filterDirContents path fs
+
+getFileSize :: FileStatus -> Int
+getFileSize stat = read (show (fileSize stat)) :: Int
+
+getDirSize :: FileStatus -> Int
+getDirSize = undefined
